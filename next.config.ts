b823202613b4f2d9bbe5aa473b27fa5ad426ee09
rohitdevc <rohitc129@gmail.com>
2025-11-13 13:17:11 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/staging',
-  assetPrefix: '/staging/',
-  trailingSlash: false,
 };
 
 export default nextConfig;
